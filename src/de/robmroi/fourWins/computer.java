e31edaa9864042c7c9d5ab345f83c85ce578ec84
Max Rinke <maxrinke@users.noreply.github.com>
2018-02-07 21:32:12 +0100

    Ich habe dieses Computer dings geamcht
    computer dings halt

--- src/de/robmroi/fourWins/computer.java
+++ src/de/robmroi/fourWins/computer.java
@@ -1,4 +1,28 @@
 package de.robmroi.fourWins;
 //Its an computer that plays against you!
 public class computer {
+        // WENN DER COMPUTER CHECKEN SOLL, OB ER GEWINNEN KANN, DANN MUSS int player 2 sein.
+        // WENN DER COMPUTER CHECKEN SOLL, OB ER DEN SPIELZUG DES GEGENSPIELERS VEREITELN KANN, MUSS int player 1 sein.
+
+                    System.out.println("< Diagonal >");
+                }
+            }
+        }
+
+        for(int y = 5; y>2; y--){
+            for(int x = 0; x<4; x++){
+                if(places[x][y] == player && places[x+1][y-1] == player && places[x+2][y-2] == player) {
+
+                    out places[x+3][y-3] = computerState;
+
+                    System.out.println("< Diagonale (andersherum) >");
+                }
+            }
+        }
+        return 0;
+    }
+
+
+
+
 }
